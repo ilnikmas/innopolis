388dae8d87427b90379a58f22a0e13b73dfced22
Ilya Maslenchenko <ilnikmas@tut.by>
2020-06-09 17:49:24 +0300

    Устранение замечаний

--- src/ru/maslenchenko/ilya/lesson03/Task4.java
+++ src/ru/maslenchenko/ilya/lesson03/Task4.java
@@ -14,13 +14,14 @@ public class Task4 {
         int guessedNumber = random.nextInt(100) + 1 ; //Диапазон загадываемых чисел от 1 до 100 включительно
         Scanner scanner = new Scanner(System.in);
         int initialDifference = guessedNumber;
-        System.out.println("I'm thinking of a number from 1 to 100. You have 10 attempts to guess it.");
+        System.out.println("I'm thinking of a number from 1 to 100." +
+                " You have 10 attempts to guess it.\nFor exit enter number out of range 1 - 100");
         for (int i = 0; i < 10; i++) {
             try {
                 System.out.format("Attempt %d. Input number:%n", i + 1);
                 int tryNumber = Integer.parseInt(scanner.next());
                 int currentDifference = Math.abs(guessedNumber - tryNumber);
-                if ((tryNumber >= 1) & (tryNumber <= 100)) {
+                if (tryNumber >= 1 && tryNumber <= 100) {
                     if (tryNumber == guessedNumber) {
                         System.out.print("You win!!!");
                         return;
@@ -31,8 +32,8 @@ public class Task4 {
                     }
                     initialDifference = currentDifference;
                 } else {
-                    System.out.println("Your answer is out of range 1-100. Try again.");
-                    i--;
+                    System.out.println("Your answer is out of range 1-100. Exit of program.");
+                    return;
                 }
             } catch (NumberFormatException e) {
                 System.out.println("You have entered not integer! Try again.");
