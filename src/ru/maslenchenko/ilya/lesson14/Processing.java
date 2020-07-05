d4b28eb860c5b3a8169cd1e09ddc88128cf46700
Ilya Maslenchenko <ilnikmas@tut.by>
2020-07-05 18:50:50 +0300

    Lesson14 Task#1

---
+++ src/ru/maslenchenko/ilya/lesson14/Processing.java
@@ -0,0 +1,90 @@
+package ru.maslenchenko.ilya.lesson14;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Processing {
+    private static final Logger logger = LogManager.getLogger(Processing.class);
+
+    public static void printInterface() {
+        logger.trace("Method printInterface() started");
+        System.out.println("Для просмотра меню нажмите 1\nЧтобы внести деньги, нажмите 2\nДля выбора напитка нажмите 3");
+        System.out.println("Для проверки остатка денежных средств нажмите 4");
+        System.out.println("Для завершения работы нажмите 0");
+    }
+
+    public static void printMenu() {
+        for (Assortment a : Assortment.values()) {
+            System.out.printf("%d: %s, цена %.2f\n", a.getButton(), a.getDrink(), a.getPrice());
+        }
+    }
+
+    public static int drinkChoice() {
+        System.out.println("Для выбора напитка нажмите цифру, соответствующую номеру напитка в меню:");
+        return inputIntWithCheck(0, 9);
+
+    }
+    public static double drinkOut(int drink, double deposit) {
+        double price = 0;
+        if (deposit > 0) {
+            for (Assortment a : Assortment.values()) {
+                if (a.getButton() == drink) {
+                    if (a.getPrice() <= deposit) {
+                        System.out.printf("Получите свой напиток: %s\n\n\n", a.getDrink());
+                        price = a.getPrice();
+                    } else System.out.println("Недостаточно денег на счёте. Пополните счёт.");
+                }
+            }
+        } else System.out.println("На вашем счёте отсутствуют средства. Пополните счёт.");
+        return price;
+    }
+
+    static int inputIntWithCheck(int min, int max) {
+        boolean flag = true;
+        Scanner scanner = null;
+        int number = 0;
+        //System.out.printf("Введите цифру от %d до %d:\n", min, max);
+        do {
+            scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                if (input < min || input > max) {
+                    System.out.println("Введёное число находится вне заданного диапазона. Ещё раз: ");
+                    flag = true;
+                } else {
+                    number = input;
+                    flag = false;
+                }
+            } else {
+                System.out.println("Вводить нужно целое число. Ещё раз: ");
+                flag = true;
+            }
+        } while (flag);
+        return number;
+    }
+
+    static double inputDoubleWithCheck() {
+        boolean flag = true;
+        Scanner scanner = null;
+        double number = 0;
+        System.out.println("Введите сумму: ");
+
+        do {
+            scanner = new Scanner(System.in);
+            scanner.useLocale(Locale.US);
+            String inputStr = scanner.nextLine();
+            try {
+                double input = Double.parseDouble(inputStr.replace(',','.'));
+                number = input;
+                flag = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Вводить нужно число. Ещё раз: ");
+                flag = true;
+            }
+        } while (flag);
+        return number;
+    }
+}
---
+++ src/ru/maslenchenko/ilya/lesson14/Run.java
@@ -0,0 +1,40 @@
+package ru.maslenchenko.ilya.lesson14;
+import static ru.maslenchenko.ilya.lesson14.Processing.*;
+
+public class Run {
+    public static void main(String[] args) {
+        int choice = 0;
+        double deposit = 0;
+        do {
+            printInterface();
+            choice = inputIntWithCheck(0, 4);
+            switch (choice) {
+                case 0: {
+                    //Выход из программы
+                    return;
+                }
+                case 1: {
+                    //Отображение меню напитков
+                    printMenu();
+                    break;
+                }
+                case 2: {
+                    //Пополнение счёта
+                    deposit += inputDoubleWithCheck();
+                    break;
+                }
+                case 3: {
+                    //Выбор и выдача напитка, а также уменьшение суммы на счёте
+                    deposit -= drinkOut(drinkChoice(), deposit);
+                    break;
+                }
+                case 4: {
+                    //Проверка баланса на счёте
+                    System.out.println("Остаток на счёте: " + deposit);
+                }
+            }
+
+        } while (true);
+    }
+}
+
