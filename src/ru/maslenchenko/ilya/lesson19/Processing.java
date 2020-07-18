1133fa85055a5537cb89b8051299f8b05dc0966b
Ilya Maslenchenko <ilnikmas@tut.by>
2020-07-18 18:21:02 +0300

    Финальные правки

--- src/ru/maslenchenko/ilya/lesson19/Processing.java
+++ src/ru/maslenchenko/ilya/lesson19/Processing.java
@@ -3,14 +3,20 @@ package ru.maslenchenko.ilya.lesson19;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Processing {
     static List<Bill> list = new ArrayList<>();
-    public static void readFile() throws IOException {
+
+    static String checkDouble(Double value) {
+        if (value % 1 == 0)
+            return String.format("%.0f", value);
+        else
+            return String.format("%.3f", value);
+    }
+
+    static void readFile() throws IOException {
         Path path = Paths.get("D:\\coding\\products.txt");
         FileReader fr = new FileReader(String.valueOf(path));
         BufferedReader reader = new BufferedReader(fr);
@@ -36,17 +42,17 @@ public class Processing {
         formatOutput();
     }
 
-    public static void formatOutput() {
-        DecimalFormat df3 = new DecimalFormat("#.###");
-        df3.setDecimalSeparatorAlwaysShown(false);
+    static void formatOutput() {
         double sum = 0;
-        System.out.println("Наименование\t\t\tЦена\tКол-во\t\tСтоимость");
-        System.out.println("=====================================================");
+        System.out.printf("%-24s%-8s%-8s%12s%n", "Наименование", "Цена", "Кол-во", "Стоимость");
+        System.out.println("====================================================");
         for (Bill b : list) {
             sum += b.getCost();
-            System.out.printf("%-15s\t\t%8.2f x %7.3f\t =%10.2f\n", b.getName(), b.getPrice(), b.getWeight(), b.getCost());
+            System.out.printf("%-22s%6.2f x %6s%5s%10.2f%n", b.getName(), b.getPrice(), checkDouble(b.getWeight()), "=", b.getCost());
         }
-        System.out.println("=====================================================");
-        System.out.printf("Итого:\t\t\t\t\t\t\t\t\t%12.2f", sum);
+        System.out.println("====================================================");
+        System.out.printf("%-45s%6.2f", "Итого:", sum);
     }
+
+
 }
